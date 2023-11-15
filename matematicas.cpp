8aa9214a6b24bf9d6c94f63a479d3f7e57869eb1
fferrlop <fferrlop@myuax.com>
2023-11-15 19:10:35 +0100

    Final 1

--- matematicas.cpp
+++ matematicas.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+//Funciones
+
 namespace Matematicas {
 
     int suma(int a, int b) {
@@ -37,9 +39,24 @@ namespace Matematicas {
     }
 }
 
+
+//Operaciones
+
     int main() {
 
         std::cout << "Suma: " << Matematicas::suma(8, 2) << std::endl;
+
+        std::cout << "Resta: " << Matematicas::resta(6, 3) << std::endl;
+
+        std::cout << "Multiplicacion: " << Matematicas::multiplicacion(4, 2) << std::endl;
+
+        std::cout << "Division: " << Matematicas::division(12, 3) << std::endl;
+
+        std::cout << "Potencia: " << Matematicas::potencia(2, 3) << std::endl;
+
+        std::cout << "Raiz: " << Matematicas::raiz(16) << std::endl;
+
+
 
         return 0;
     }
