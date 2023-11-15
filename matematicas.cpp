4858b4a1f3ef7943463cfb43102a138fb218045f
fferrlop <fferrlop@myuax.com>
2023-11-15 18:56:59 +0100

    +-*/

---
+++ matematicas.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Francisco on 15/11/2023.
+//
+
+#include "matematicas.h"
+
+
+namespace Matematicas {
+
+    int suma(int a, int b) {
+        return a + b;
+    }
+
+    int resta(int a, int b) {
+        return a - b;
+    }
+
+    int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    int division(int a, int b) {
+        return a / b;
+    }
+
+
+}
---
+++ matematicas.h
@@ -0,0 +1,14 @@
+//
+// Created by Francisco on 15/11/2023.
+//
+
+#ifndef EJERCICIOS_STL_MATEMATICAS_H
+#define EJERCICIOS_STL_MATEMATICAS_H
+
+
+class matematicas {
+
+};
+
+
+#endif //EJERCICIOS_STL_MATEMATICAS_H
