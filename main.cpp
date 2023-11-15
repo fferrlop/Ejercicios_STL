dffaabb3d96b9754dab1c413af9165ef26c3a833
fferrlop <fferrlop@myuax.com>
2023-11-15 19:06:27 +0100

    now it works

--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
-#include "matematicas.cpp"
 
-int main() {
+namespace Matematicas {
 
-    std::cout << "Suma: " << Matematicas::suma(8,2) << std::endl;
+    int suma(int a, int b) {
+        return a + b;
+    }
+
+    int resta(int a, int b) {
+        return a - b;
+    }
+
+    int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    int division(int a, int b) {
+        return a / b;
+    }
+
+    int potencia(int a, int b) {
+        int resultado = 1;
+        for (int i = 0; i < b; i++) {
+            resultado *= a;
+        }
+        return resultado;
+    }
+
+    int raiz(int a) {
+        int resultado = 0;
+        for (int i = 0; i < a; i++) {
+            if (i * i == a) {
+                resultado = i;
+            }
+        }
+        return resultado;
+    }
 }
+
+    int main() {
+
+        std::cout << "Suma: " << Matematicas::suma(8, 2) << std::endl;
+
+        return 0;
+    }
 
